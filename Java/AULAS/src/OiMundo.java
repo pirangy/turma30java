7b379404d9282e96b2c6d4384a99d2cbed6009c0
pirangy <pirangy.ariel@gmail.com>
2021-08-02 09:26:49 -0300

    Exercício em Grupo - E-Commerce - Organização de Pastas

---
+++ Java/AULAS/src/OiMundo.java
@@ -0,0 +1,17 @@
+
+public class OiMundo 
+{
+/* programa {
+ * 	funcao inicio (){
+ * 		}
+ * }*/
+	public static void main (String[] args) 
+	{
+		
+		System.out.print("Oi, Mundo!!");
+		
+		
+		System.out.println("\nMeu nome é Ariel!!");
+		
+	}
+}
