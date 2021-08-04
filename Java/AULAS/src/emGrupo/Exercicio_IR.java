cf6458998abb497c60688ff43bf7e3d2d2dcf6f8
pirangy <pirangy.ariel@gmail.com>
2021-08-03 22:25:48 -0300

    Exercício IR - Concluído

--- Java/AULAS/src/emGrupo/Exercicio_IR.java
+++ Java/AULAS/src/emGrupo/Exercicio_IR.java
@@ -25,18 +25,26 @@ public class Exercicio_IR {
 		if(id == 1)
 			{
 				pronome = "Sr.";
-				System.out.println("Olá, Sr. "+nome);
+				System.out.println("Olá, Sr. "+nome+"! Seja bem-vindo!");
+
 			}
 		else if(id == 2)
 			{
 				pronome = "Sra.";
-				System.out.println("Olá, Sra. "+nome);
+				System.out.println("Olá, Sra. "+nome+"! Seja bem-vinda!");
 			}
 		else if (id == 3)
 			{
 				pronome = "Sre.";
-				System.out.println("Olá, Sre. "+nome);
+				System.out.println("Olá, Sre. "+nome+"! Seja bem-vinde!");
 			}
+		else 
+			{
+			
+				System.out.println("Desculpe, essa opção não está registrada. Te ajudamos a editar o cadastro depois ;)");
+			
+			}
+		
 		
 		//Entradas 2: salário de usuáries
 		System.out.println("Por favor, digite seu salário bruto: R$ ");
@@ -47,23 +55,23 @@ public class Exercicio_IR {
 		//Saídas
 		if (salario<=2000) 
 			{
+			
+					System.out.println(nome+", seu salário está isento do imposto de renda.");
 				
-				System.out.println("Seu salário está isento do imposto de renda."+nome);
-			
 			}   
-		if (salario>2000 && salario<=5000) 
+		else if (salario>2000 && salario<=5000) 
 			{
-
+			
 				double imp15=salario*0.15;
-				System.out.println("Seu IR é: "+imp15);	
-			
+				System.out.println(nome+", seu IR é: R$"+imp15);
+				
 			}	
-		else 
+		else
 			{
-			
+				
 				double imp25=salario*0.25;			
-				System.out.printf("Seu IR é:"+imp25);
-			
+				System.out.printf(nome+", seu IR é: R$"+imp25);
+				
 			}
 	}
 }
