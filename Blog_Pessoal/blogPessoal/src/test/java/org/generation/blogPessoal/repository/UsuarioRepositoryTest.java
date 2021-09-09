b723c79e65c38d5c782b488c887217d29582458a
pirangy <pirangy.ariel@gmail.com>
2021-09-09 15:02:31 -0300

    Blog Pessoal - JUnit - UsuarioControllerTest & Ajustes em UsuarioController

--- Blog_Pessoal/blogPessoal/src/test/java/org/generation/blogPessoal/repository/UsuarioRepositoryTest.java
+++ Blog_Pessoal/blogPessoal/src/test/java/org/generation/blogPessoal/repository/UsuarioRepositoryTest.java
@@ -1,6 +1,7 @@
 package org.generation.blogPessoal.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.text.ParseException;
